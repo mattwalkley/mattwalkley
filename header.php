e4923c8285a1e1aa00adce30baec56543d947bc2
Matt Walkley <matt@mattwalkley.com>
2013-07-18 00:25:51 -0400

    Improved mobile performance site-wide

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
    <meta charset="utf-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
    <meta name="description" content="">
-   <meta name="viewport" content="width=device-width,initial-scale=1">
+   <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
    <meta name="apple-mobile-web-app-capable" content="yes">
    <link rel="shortcut icon" href="favicon.png">
 
